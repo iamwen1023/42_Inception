0e215ad7213014c90fbb84d879a8508081ce91ea
iamwen1023 <ursula771023@gmail.com>
2023-01-03 15:44:22 +0100

    wordpress

--- wordpress/tools/auto_config.sh
+++ wordpress/tools/auto_config.sh
@@ -5,4 +5,8 @@ wp config create	--allow-root \
 					--dbuser=$SQL_USER \
 					--dbpass=$SQL_PASSWORD \
 					--dbhost=mariadb:3306 --path='/var/www/wordpress'
-https://developer.wordpress.org/cli/commands/core/install/
+wp core install --url=example.com \
+				--title=Example \
+				--admin_user=supervisor \
+				--admin_password=strongpassword \
+				--admin_email=info@example.com
