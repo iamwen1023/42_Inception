5ef09e9c53175935a15c7072e2a2e066b327240c
iamwen1023 <ursula771023@gmail.com>
2023-01-04 16:32:35 +0100

    wordpress install

--- wordpress/tools/auto_config.sh
+++ wordpress/tools/auto_config.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 sleep 10
-wp config create	--allow-root \
-					--dbname=$SQL_DATABASE \
-					--dbuser=$SQL_USER \
-					--dbpass=$SQL_PASSWORD \
-					--dbhost=mariadb:3306 --path='/var/www/wordpress'
-wp core install --url=example.com \
-				--title=Example \
-				--admin_user=supervisor \
-				--admin_password=strongpassword \
-				--admin_email=info@example.com
+wp config create --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD --dbhost=mariadb:3306 --path='/var/www/wordpress'
+wp db create
+wp core install --url=example.com --title=Example --admin_user=supervisor --admin_password=strongpassword --admin_email=info@example.com
