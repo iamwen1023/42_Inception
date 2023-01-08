6b8ab46c2907a59978dc3fe93bd5b28e1a19ed1d
iamwen1023 <ursula771023@gmail.com>
2023-01-08 16:52:36 +0100

    wordpress [200~ERROR 1045 (28000): Access denied for user '

--- mariadb/tools/db_config.sh
+++ mariadb/tools/db_config.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-service mysql start;
+#service mysql start;
 
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
--- wordpress/tools/auto_config.sh
+++ wordpress/tools/auto_config.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 sleep 10
-wp config create --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD --dbhost=mariadb:3306 --path='/var/www/wordpress'
-wp db create
-wp core install --url=example.com --title=Example --admin_user=supervisor --admin_password=strongpassword --admin_email=info@example.com
+
+wp
+wp core download --allow-root
+wp core config --allow-root --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASSWORDs --dbhost=mariadb:3306  --path='/var/www/wordpress' 
+wp db create  --allow-root  --path='/var/www/wordpress'
+wp core install --allow-root  --url=example.com --title=Example --admin_user=supervisor --admin_password=strongpassword --admin_email=info@example.com  --path='/var/www/wordpress'
+
+./usr/sbin/php-fpm7.3 --nodaemonize
